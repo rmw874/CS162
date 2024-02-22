fe09099fd663ac3cc7bf3e3ae2f84b684c62455e
Oscar Heiberg <rmw874@alumni.ku.dk>
2024-02-22 04:21:28 -0800

    done. the error messages might be wrong, but that is not the biggest concern right now

--- homework/hw4/lib/lamp/typecheck.ml
+++ homework/hw4/lib/lamp/typecheck.ml
@@ -30,91 +30,112 @@ let rec equal_ty (t1 : ty) (t2 : ty) : bool = match (t1, t2) with
 
 (**
 type expr =
+  (* arithmetic *)
   | Num of int
   | Binop of binop * expr * expr
+  (* binding *)
   | Var of string
   | Scope of string * expr
+  (* lambda calculus *)
   | Lambda of ty option * expr
   | App of expr * expr
+  (* let expression *)
   | Let of expr * expr
+  (* booleans *)
   | True
   | False
   | IfThenElse of expr * expr * expr
   | Comp of relop * expr * expr
+  (* lists *)
   | ListNil of ty option
   | ListCons of expr * expr
   | ListMatch of expr * expr * expr
+  (* fix *)
   | Fix of ty option * expr
+  (* products *)
   | Pair of expr * expr
   | Fst of expr
   | Snd of expr
-  | Annot of expr * ty    
+  (* type annotation *)
+  | Annot of expr * ty   
 *)
 let rec abstract_eval (gamma : gamma) (e : expr) : ty =
   try
     match e with
-    (**Num of int*)
+    (* arithmetic *)
     | Num _ -> TInt
-    (**Binop of binop * expr * expr*)
     | Binop (_, e1, e2) -> (match (abstract_eval gamma e1, abstract_eval gamma e2) with
                             | (TInt, TInt) -> TInt (** if both operands are integers, then the result is an integer *)
-                            | _ -> ty_err ("[synth] ill-formed binary operation"))
-    (**Var of string*)
+                            | _ -> ty_err ("[synth] ill-formed binary operation" ^ show_expr e))
+
+    (* binding *)
     | Var x -> (match find gamma x with
                 | Some t -> t (** if the variable is in the environment, then return its type *)
                 | None -> ty_err ("[synth] undefined variable: " ^ x))
-    (**Scope of string * expr*)
     | Scope (x, e) -> abstract_eval (add gamma x (abstract_eval gamma e)) e (** add the type and the variable to the environment and evaluate the expression *)
-    (**Lambda of ty option * expr*)
+
+    (* lambda calculus *)
     | Lambda (t, e) -> (match t with
                         | Some t1 -> TFun (t1, abstract_eval gamma e) (** if the type is given, then the type of the lambda is the function type *)
-                        | None -> ty_err ("[synth] ill-formed lambda"))
-    (**Let of expr * expr*)
+                        | None -> ty_err ("[synth] ill-formed lambda" ^ show_expr e))
+    | App (e1, e2) -> (match (abstract_eval gamma e1, abstract_eval gamma e2) with
+                      | (TFun (t1, t2), t3) -> (match equal_ty t1 t3 with
+                                                | true -> t2 (** if the type of the function is the same as the type of the argument, then the type of the application is the type of the output *)
+                                                | false -> ty_err ("[synth] ill-formed application" ^ show_expr e))
+                      | _ -> ty_err ("[synth] ill-formed application" ^ show_expr e))
+
+    (* let expression *)
     | Let (e1, e2) -> abstract_eval (add gamma "v" (abstract_eval gamma e1)) e2
-    (**App of expr * expr*)
-    | App (e1, e2) -> todo() (**! TODO *)
-    (**True*)
     | True -> TBool
-    (**False*)
     | False -> TBool
-    (**IfThenElse of expr * expr * expr*)
     | IfThenElse (e1, e2, e3) -> (match (abstract_eval gamma e1, abstract_eval gamma e2, abstract_eval gamma e3) with
-                                  | (TBool, t2, t3) -> if equal_ty t2 t3 then t2 else ty_err ("[synth] ill-formed if-then-else") (** if the condition is a boolean, then the types of the branches must be the same *)
-                                  | _ -> ty_err ("[synth] ill-formed if-then-else"))
-    (**Comp of relop * expr * expr*)
+                                  | (TBool, t2, t3) -> (match equal_ty t2 t3 with
+                                                        | true -> t2 (** if the condition is a boolean, then the types of the branches must be the same (t1 = t2) *)
+                                                        | false -> ty_err ("[synth] ill-formed if-then-else" ^ show_expr e))
+                                  | _ -> ty_err ("[synth] ill-formed if-then-else" ^ show_expr e))
     | Comp (_, e1, e2) -> (match (abstract_eval gamma e1, abstract_eval gamma e2) with
                           | (TInt, TInt) -> TBool (** if both operands are integers, then the result is a boolean *)
-                          | _ -> ty_err ("[synth] ill-formed comparison"))
-    (**ListNil of ty option*)
+                          | _ -> ty_err ("[synth] ill-formed comparison" ^ show_expr e))
+
+    (* lists *)
     | ListNil (t) -> (match t with
                       | Some t1 -> TList t1 (** if the type is given, then the type of the list is the type of the elements *)
-                      | None -> ty_err ("[synth] ill-formed list nil"))
+                      | None -> ty_err ("[synth] ill-formed listnil" ^ show_expr e))
     | ListCons (e1, e2) -> (match abstract_eval gamma e1 with
                             | t1 -> (match abstract_eval gamma e2 with
-                                     | TList t2 -> if equal_ty t1 t2 then TList t1 else ty_err ("[synth] ill-formed list cons") (** if the types of the elements are the same, then the type of the list is the type of the elements *)
-                                     | _ -> ty_err ("[synth] ill-formed list cons"))
-                            | _ -> ty_err ("[synth] ill-formed list cons"))
-    (**ListMatch of expr * expr * expr*)
+                                     | TList t2 -> (match equal_ty t1 t2 with
+                                                    | true -> TList t1
+                                                    | false -> ty_err ("[synth] ill-formed list cons" ^ show_expr e2))
+                                     | _ -> ty_err ("[synth] ill-formed list cons" ^ show_expr e2)))
     | ListMatch (e1, e2, e3) -> (match abstract_eval gamma e1 with
                                   | TList _ -> let t2 = abstract_eval gamma e2 in
                                                let t3 = abstract_eval gamma e3 in
-                                               if equal_ty t2 t3 then t2 else ty_err ("[synth] ill-formed list match")
-                                  | _ -> ty_err ("[synth] ill-formed list match"))
-    (**Fix of ty option * expr*)
+                                               (match equal_ty t2 t3 with
+                                                | true -> t2
+                                                | false -> ty_err ("[synth] ill-formed listmatch" ^ show_expr e3))
+                                  | _ -> ty_err ("[synth] ill-formed listmatch" ^ show_expr e1))
+
+    (* fix *)
     | Fix (t, e) -> (match t with
-                     | Some t1 -> if equal_ty t1 (abstract_eval gamma e) then t1 else ty_err ("[synth] ill-formed fix") (** if the type is given, then the type of the fix is the same as the given type *)
-                     | None -> ty_err ("[synth] ill-formed fix"))
-    (**Pair of expr * expr*)
+                     | Some t1 -> (match equal_ty t1 (abstract_eval gamma e) with 
+                                   | true -> t1 (** if the type is given and the type of the expression is the same, then the type of the fix is the given type *)
+                                   | false -> ty_err ("[synth] ill-formed fix" ^ show_expr e))
+                     | None -> ty_err ("[synth] ill-formed fix" ^ show_expr e)) (** if the type is not given, then the type of the fix is the type of the expression *)
+
+    (* products *)
     | Pair (e1, e2) -> TProd (abstract_eval gamma e1, abstract_eval gamma e2)
-    (**Fst of expr*)
     | Fst e -> (match abstract_eval gamma e with
                 | TProd (t1, _) -> t1 (** the type of the first projection is the type of the first element of the pair *)
-                | _ -> ty_err ("[synth] ill-formed first projection"))
-    (**Snd of expr*)
+                | _ -> ty_err ("[synth] ill-formed fst" ^ show_expr e))
     | Snd e -> (match abstract_eval gamma e with
                 | TProd (_, t2) -> t2 (** the type of the second projection is the type of the second element of the pair *)
-                | _ -> ty_err ("[synth] ill-formed second projection"))
-    (**Annot of expr * ty*)
-    | Annot (e, t) -> todo() (**! TODO *)
+                | _ -> ty_err ("[synth] ill-formed snd projection" ^ show_expr e))
+
+    (* type annotation *)
+    | Annot (e, t) -> (match abstract_eval gamma e with
+                      | t1 -> (match equal_ty t t1 with
+                               | true -> t
+                               | false -> ty_err ("[synth] ill-formed annotation" ^ show_expr e)) (** if the type of the expression is the same as the given type, then the type of the expression is the given type *)
+                      )
     | _ -> ty_err ("[synth] ill-formed: " ^ show_expr e)
   with Type_error msg -> ty_err (msg ^ "\nin expression " ^ show_expr e)
